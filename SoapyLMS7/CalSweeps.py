e3f26a8dfa9a949e8a74829fd0e552ee2b1d6578
Josh Blum <josh@skylarkwireless.com>
2018-05-28 13:23:36 -0500

    sharing scripts for measuring impariments

---
+++ SoapyLMS7/CalSweeps.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('/usr/local/lib/python3/dist-packages')
+import SoapySDR
+from SoapySDR import * #constants
+import numpy as np
+from PlotUtils import *
+import time
+import os
+
+FREQ_STEP = 100e6
+FREQS = np.concatenate([
+    np.arange(500e6, 1e9, FREQ_STEP),
+    np.arange(1e9, 2e9, FREQ_STEP),
+    np.arange(2e9, 3e9, FREQ_STEP),
+    np.arange(3e9, 3.8e9, FREQ_STEP),
+])
+TX_LO_OFS = 5.3e6
+TX_NCO = 2.1e6
+SAMP_RATE = 20e6
+BW = 30e6
+FFT_SIZE = 2048
+RX_DC_SETTLE = 0.3 #seconds is enough
+PAD = 20.0
+LNA = 30.0
+
+CAL_PAD_LEVELS = dict()
+
+def getRxPowerSpectrum(sdr, rxStream, fftSize):
+    sdr.activateStream(rxStream, SOAPY_SDR_END_BURST, 0, fftSize)
+    rxSamps = np.zeros(fftSize, np.complex64)
+    r = sdr.readStream(rxStream, [rxSamps], fftSize, timeoutUs=1000000)
+    if r.ret == SOAPY_SDR_TIMEOUT: raise Exception('readStream bug, got timeout!')
+    try: assert(r.ret == fftSize)
+    except: print(r); raise
+    if sdr.getDriverKey() != 'iris':
+        sdr.deactivateStream(rxStream) #FIXME limesuite: not automatic w/ end burst in limesdr
+    return LogPowerFFT(rxSamps)
+
+def collectSweepData(argsStr, channel=0, calibrate=False, dumpDir=None):
+    sdr = SoapySDR.Device(argsStr)
+    rxStream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32, [channel], dict(skipCal='true'))
+    rows = list()
+
+    #constant settings, setup once
+    if sdr.getDriverKey() == 'iris':
+        sdr.setAntenna(SOAPY_SDR_RX, channel, "RX")
+        sdr.setAntenna(SOAPY_SDR_TX, channel, "TRX")
+    sdr.setSampleRate(SOAPY_SDR_RX, channel, SAMP_RATE)
+    sdr.setSampleRate(SOAPY_SDR_TX, channel, SAMP_RATE)
+    sdr.setBandwidth(SOAPY_SDR_RX, channel, BW)
+    sdr.setBandwidth(SOAPY_SDR_TX, channel, BW)
+    constKey = 'TSP_TSG_CONST' if sdr.getDriverKey() == 'iris' else 'TSP_CONST'
+    sdr.writeSetting(SOAPY_SDR_TX, channel, constKey, str(1 << 13))
+
+    #per frequency settings
+    for freq in FREQS:
+        print('Testing freq = %g MHz'%(freq/1e6))
+        sdr.setGain(SOAPY_SDR_TX, channel, 'PAD', PAD)
+        sdr.setGain(SOAPY_SDR_RX, channel, 'LNA', LNA)
+        sdr.setFrequency(SOAPY_SDR_RX, channel, "RF", freq)
+        sdr.setFrequency(SOAPY_SDR_TX, channel, "RF", freq+TX_LO_OFS)
+        sdr.setFrequency(SOAPY_SDR_RX, channel, 'BB', 0.0)
+        sdr.setFrequency(SOAPY_SDR_TX, channel, 'BB', 0.0)
+
+        #calibrate PAD
+        if freq in CAL_PAD_LEVELS:
+            sdr.setGain(SOAPY_SDR_TX, channel, 'PAD', CAL_PAD_LEVELS[freq])
+        else:
+            psRx = getRxPowerSpectrum(sdr, rxStream, FFT_SIZE)
+            rxLvl = MeasureTonePower(psRx, SAMP_RATE, TX_LO_OFS)
+            newPad = PAD+(-20-rxLvl) #-20db tone level
+            if newPad > 50: newPad = 50 #clip
+            sdr.setGain(SOAPY_SDR_TX, channel, 'PAD', newPad)
+            CAL_PAD_LEVELS[freq] = newPad #save for next time
+            print('orig level = %g dB, new PAD value = %g dB'%(rxLvl, newPad))
+
+        if calibrate:
+            sdr.writeSetting(SOAPY_SDR_RX, channel, 'CALIBRATE', str(BW))
+            #tx cal can fail due to weak pad gain for internal loopback
+            #try higher gains to get it to pass if it initially fails
+            keepGoing = True
+            while keepGoing:
+                try:
+                    sdr.writeSetting(SOAPY_SDR_TX, channel, 'CALIBRATE', str(BW))
+                    break
+                except:
+                    pad = sdr.getGain(SOAPY_SDR_TX, channel, 'PAD') + 5
+                    if pad > 50: keepGoing = False
+                    print('Failed to cal, trying pad = %g dB'%(pad))
+                    sdr.setGain(SOAPY_SDR_TX, channel, 'PAD', pad)
+            else:
+                raise Exception('Failed to cal tx, gain problems...')
+            sdr.setGain(SOAPY_SDR_TX, channel, 'PAD', CAL_PAD_LEVELS[freq]) #restore
+
+        #intentionally disable digital removal to measure the analog removal
+        sdr.setDCOffsetMode(SOAPY_SDR_RX, channel, False)
+
+        #data collections
+        psRx = getRxPowerSpectrum(sdr, rxStream, FFT_SIZE)
+        sdr.setDCOffsetMode(SOAPY_SDR_RX, channel, True) #enable digital dc removal again for the next plot
+        if calibrate: time.sleep(RX_DC_SETTLE) #settle digital removal
+        sdr.setFrequency(SOAPY_SDR_TX, channel, 'BB', TX_NCO)
+        psTx = getRxPowerSpectrum(sdr, rxStream, FFT_SIZE)
+        rxLvl = MeasureTonePower(psRx, SAMP_RATE, TX_LO_OFS)
+        rxIq = MeasureTonePower(psRx, SAMP_RATE, -TX_LO_OFS)
+        rxDc = MeasureTonePower(psRx, SAMP_RATE, 0.0)
+        txLvl = MeasureTonePower(psTx, SAMP_RATE, TX_LO_OFS+TX_NCO)
+        txIq = MeasureTonePower(psTx, SAMP_RATE, TX_LO_OFS-TX_NCO)
+        txDc = MeasureTonePower(psTx, SAMP_RATE, TX_LO_OFS)
+        rxDcDig = MeasureTonePower(psTx, SAMP_RATE, 0.0)
+
+        rows.append([freq, psRx, rxLvl, rxIq, rxDc, psTx, txLvl, txIq, txDc, rxDcDig])
+
+        #debug dumps to file
+        if dumpDir:
+            try: os.makedirs(dumpDir)
+            except: pass
+            fig = LocalFigure(size=[2, 1], title='Rx and Tx impairments %g MHz, PAD=%g dB'%(freq/1e6, CAL_PAD_LEVELS[freq]))
+            fig.addFreqPlot(1, traces=[psRx], rate=SAMP_RATE, title='Tx CW tone into Rx')
+            fig.addFreqPlot(2, traces=[psTx], rate=SAMP_RATE, title='Tx w/ NCO into Rx')
+            out = os.path.join(dumpDir, 'plot_%04d.png'%int(freq/1e6))
+            print(out)
+            fig.savefig(out)
+            fig.close()
+
+    sdr.closeStream(rxStream)
+    sdr = None
+    return rows
+
+def main():
+    argsStr = sys.argv[1]
+    dumpDir = sys.argv[2]
+
+    default_rows = collectSweepData(argsStr, calibrate=False, dumpDir=os.path.join(dumpDir, 'default'))
+    calibrated_rows = collectSweepData(argsStr, calibrate=True, dumpDir=os.path.join(dumpDir, 'calibrated'))
+
+    rxLvls = list()
+    rxIqsInitial = list()
+    rxIqsCorrected = list()
+    rxDcsInitial = list()
+    rxDcsCorrected = list()
+    rxDcsCorrDigital = list()
+    txLvls = list()
+    txIqsInitial = list()
+    txIqsCorrected = list()
+    txDcsInitial = list()
+    txDcsCorrected = list()
+
+    imagesList = list()
+    for default_row, calibrated_row in zip(default_rows, calibrated_rows):
+        freq, psRx_DEF, rxLvl_DEF, rxIq_DEF, rxDc_DEF, psTx_DEF, txLvl_DEF, txIq_DEF, txDc_DEF, rxDcDig_DEF = default_row
+        freq, psRx_CAL, rxLvl_CAL, rxIq_CAL, rxDc_CAL, psTx_CAL, txLvl_CAL, txIq_CAL, txDc_CAL, rxDcDig_CAL = calibrated_row
+        rxLvls.append(rxLvl_CAL)
+        rxIqsInitial.append(rxIq_DEF)
+        rxIqsCorrected.append(rxIq_CAL)
+        rxDcsInitial.append(rxDc_DEF)
+        rxDcsCorrected.append(rxDc_CAL)
+        rxDcsCorrDigital.append(rxDcDig_CAL)
+        txLvls.append(txLvl_CAL)
+        txIqsInitial.append(txIq_DEF)
+        txIqsCorrected.append(txIq_CAL)
+        txDcsInitial.append(txDc_DEF)
+        txDcsCorrected.append(txDc_CAL)
+        fig = LocalFigure(size=[2, 2], title='Rx and Tx impairments %g MHz, PAD=%g dB'%(freq/1e6, CAL_PAD_LEVELS[freq]))
+        fig.addFreqPlot(1, traces=[psRx_DEF], rate=SAMP_RATE, title='Default: Tx CW tone into Rx')
+        fig.addFreqPlot(2, traces=[psTx_DEF], rate=SAMP_RATE, title='Default: Tx w/ NCO into Rx')
+        fig.addFreqPlot(3, traces=[psRx_CAL], rate=SAMP_RATE, title='Calibrated: Tx CW tone into Rx')
+        fig.addFreqPlot(4, traces=[psTx_CAL], rate=SAMP_RATE, title='Calibrated: Tx w/ NCO into Rx')
+        out = os.path.join(dumpDir, 'plot_%04d.png'%int(freq/1e6))
+        print(out)
+        imagesList.append(out)
+        fig.savefig(out)
+        fig.close()
+
+    fig = LocalFigure(size=[2, 2], title='Summary of impairments for %s'%str(argsStr))
+
+    fig.addDataPlot(1, xaxis=FREQS/1e6, traces=[
+        (rxLvls, 'Rx Level'),
+        (rxIqsInitial, 'Rx IQ Before'),
+        (rxIqsCorrected, 'Rx IQ Corrected'),
+    ], title='Rx IQ imbalance calibrations', xlabel='Frequency (MHz)', ylabel='Power (dBfs)', ylim=[-100, 0])
+
+    fig.addDataPlot(2, xaxis=FREQS/1e6, traces=[
+        (rxLvls, 'Rx Level'),
+        (rxDcsInitial, 'Rx DC Before'),
+        (rxDcsCorrected, 'Rx Dc Corrected'),
+        (rxDcsCorrDigital, 'Rx Dc w/ TSP'),
+    ], title='Rx Dc imbalance calibrations', xlabel='Frequency (MHz)', ylabel='Power (dBfs)', ylim=[-100, 0])
+
+    fig.addDataPlot(3, xaxis=FREQS/1e6, traces=[
+        (txLvls, 'Tx Level'),
+        (txIqsInitial, 'Tx IQ Before'),
+        (txIqsCorrected, 'Tx IQ Corrected'),
+    ], title='Tx IQ imbalance calibrations', xlabel='Frequency (MHz)', ylabel='Power (dBfs)', ylim=[-100, 0])
+
+    fig.addDataPlot(4, xaxis=FREQS/1e6, traces=[
+        (txLvls, 'Tx Level'),
+        (txDcsInitial, 'Tx DC Before'),
+        (txDcsCorrected, 'Tx Dc Corrected'),
+    ], title='Tx Dc imbalance calibrations', xlabel='Frequency (MHz)', ylabel='Power (dBfs)', ylim=[-100, 0])
+
+    out = os.path.join(dumpDir, 'summary.png')
+    print(out)
+    imagesList.insert(0, out)
+    fig.savefig(out)
+    fig.close()
+
+    out = os.path.join(dumpDir, 'summary.pdf')
+    print(out)
+    os.system('convert %s %s'%(' '.join(imagesList), out))
+
+if __name__ == '__main__':
+    main()
---
+++ SoapyLMS7/PlotUtils.py
@@ -0,0 +1,275 @@
+########################################################################
+## Utility functions to take FFTs and locate tones
+########################################################################
+
+import numpy as np
+import math
+import scipy.signal
+
+def LogPowerFFT(samps, numBins=None, peak=1.0, reorder=True, window=None):
+    """
+    Calculate the log power FFT bins of the complex samples.
+    When numBins is not specified, the FFT is across all samples.
+    @param samps numpy array of complex samples
+    @param numBins take FFTs of this size and average their bins
+    @param peak maximum value of a sample (floats are usually 1.0, shorts are 32767)
+    @param reorder True to reorder so the DC bin is in the center
+    @param window function or None for default flattop window
+    @return an array of real values FFT power bins
+    """
+
+    #support complex integers (2d arrays)
+    if hasattr(samps, 'shape') and len(samps.shape) > 1 and samps.shape[1] == 2:
+        scale = 1 << ((samps.dtype.itemsize*8)-1)
+        samps = np.array(list(map(lambda x: complex(*x), samps)), np.complex64)/scale
+
+    size = len(samps)
+
+    if numBins is not None:
+        numFFTs = int(size/numBins)
+        #print 'size', size
+        #print 'numBins', numBins
+        #print 'numFFTs', numFFTs
+        allBins = list()
+        for i in range(numFFTs):
+            bins = LogPowerFFT(samps[i*numBins:(i+1)*numBins], peak=peak, reorder=reorder)
+            allBins.append(np.exp(bins))
+        avgBins = sum(allBins)/numFFTs
+        return np.log(avgBins)
+
+    #scale by dividing out the peak, full scale is 0dB
+    scaledSamps = samps/peak
+
+    #calculate window
+    if not window: window = scipy.signal.hann
+    windowBins = window(size)
+    windowPower = math.sqrt(sum(windowBins**2)/size)
+
+    #apply window
+    windowedSamps = np.multiply(windowBins, scaledSamps)
+
+    #window and fft gain adjustment
+    gaindB = 20*math.log10(size) + 20*math.log10(windowPower)
+
+    #take fft
+    fftBins = np.abs(np.fft.fft(windowedSamps))
+    fftBins = np.maximum(fftBins, 1e-20) #clip
+    powerBins = 20*np.log10(fftBins) - gaindB
+
+    #bin reorder
+    if reorder:
+        idx = np.argsort(np.fft.fftfreq(len(powerBins)))
+        powerBins = powerBins[idx]
+        #even fft has two DC bins, just remove other DC bin for now
+        #if (len(powerBins) % 2) == 0: powerBins = powerBins[1:]
+
+    return powerBins
+
+def MeasureTonePower(powerBins, sampRate, toneFreq, delta=10):
+    """
+    Measure a tone level by looking for max power at a given position.
+    """
+
+    tonePosition = len(powerBins)*(toneFreq+sampRate/2)/sampRate
+    startIndex = max(0, int(tonePosition)-delta)
+    stopIndex = min(len(powerBins), int(tonePosition)+delta) #non inclusive
+
+    maxPowerIndex = startIndex
+    for i in range(startIndex, stopIndex):
+        if powerBins[i] > powerBins[maxPowerIndex]:
+            maxPowerIndex = i
+
+    return powerBins[maxPowerIndex]
+
+########################################################################
+## We are going to plot a lot of waveforms to file
+## This is basically a plot styler to avoid code duplication,
+## and to create a common style for generating the plots.
+########################################################################
+
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+import queue
+import threading
+import logging
+import traceback
+import io
+import atexit
+
+class LocalFigure(object):
+
+    def __init__(self, size=[1, 1], title=None):
+        self.fig = plt.figure(figsize=(20, 8), dpi=100)
+        self.nrows = size[0]
+        self.ncols = size[1]
+        if title is not None:
+            self.fig.suptitle(title, fontsize=16, horizontalalignment='left', x=0.01)
+        self.fig.subplots_adjust(hspace=.5, top=.85)
+
+    def addFreqPlot(self, axnum, traces, rate, title=None,
+        xlabel='Freq (MHz)', ylabel='Power (dBfs)',
+        xscale=1e6, refLvl=0.0, plotNoise=True, centerFreq=0.0, dynRange=100,
+    ):
+        ax = self.fig.add_subplot(self.nrows, self.ncols, axnum)  # specify (nrows, ncols, axnum)
+        ax.grid(True)
+        if title: ax.set_title(title, fontsize=10)
+        if xlabel: ax.set_xlabel(xlabel, fontsize=10)
+        if ylabel: ax.set_ylabel(ylabel, fontsize=10)
+        minNoise = refLvl-dynRange
+
+        for ps in traces:
+            noise, peaks = LogPowerFFTAnalysis(ps, rate)
+            #pick the N largest tones above some threshold
+            peaks = reversed(sorted(peaks, key=lambda x: x[1]))
+            peaks = [p for p in peaks if p[1] > noise + 30]
+            for freq, power in peaks[:5]:
+                label = '%.3g\n%.3g'%(freq/xscale, power)
+                x = int(ps.size*freq/rate) - ps.size/2
+                if freq > 0.0:
+                    xoff = 1.0
+                    ha = 'left'
+                else:
+                    xoff = -1.0
+                    ha = 'right'
+                if power < (refLvl-30):
+                    yoff = 1.0
+                    va = 'bottom'
+                else:
+                    yoff = -1.0
+                    va = 'top'
+                ax.annotate(label, fontsize=8,
+                    xy = (freq/xscale, power), xytext = (freq/xscale+xoff, power+yoff),
+                    horizontalalignment=ha, verticalalignment=va,
+                    xycoords = 'data', textcoords = 'data',
+                    arrowprops = dict(arrowstyle = '->'))
+                ax.scatter(freq/xscale, power, c='yellow', s=50)
+            ax.plot(np.arange((centerFreq-rate/2)/xscale, (centerFreq+rate/2)/xscale, rate/len(ps)/xscale)[:len(ps)], ps, 'b')
+            if plotNoise: ax.plot([-rate/2/xscale, rate/2/xscale], [noise, noise], 'r')
+            minNoise = min(minNoise, noise)
+            ax.set_ylim(minNoise-10, refLvl)
+
+        #10 dB step size
+        start, end = ax.get_ylim()
+        ax.yaxis.set_ticks(np.arange(start, end, 10))
+
+    def addDataPlot(self, axnum, xaxis, traces, title=None, xlabel=None, ylabel=None, ylim=None):
+        ax = self.fig.add_subplot(self.nrows, self.ncols, axnum)  # specify (nrows, ncols, axnum)
+        ax.grid(True)
+        if title: ax.set_title(title, fontsize=10)
+        if xlabel: ax.set_xlabel(xlabel, fontsize=10)
+        if ylabel: ax.set_ylabel(ylabel, fontsize=10)
+        if ylim: ax.set_ylim(*ylim)
+
+        for i, trace in enumerate(traces):
+            label = 'Trace %d'%i
+            if isinstance(trace, tuple) and len(trace) == 2:
+                trace, label = trace
+            ax.plot(xaxis, trace, label=label)
+
+        ax.legend(fontsize=10)
+
+    def savefig(self, filepath):
+        self.fig.savefig(filepath)
+
+    def close(self):
+        plt.close(self.fig)
+
+
+def peakdet(v, delta, x = None):
+    """
+    Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    
+    Returns two arrays
+    
+    function [maxtab, mintab]=peakdet(v, delta, x)
+    %PEAKDET Detect peaks in a vector
+    %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+    %        maxima and minima ("peaks") in the vector V.
+    %        MAXTAB and MINTAB consists of two columns. Column 1
+    %        contains indices in V, and column 2 the found values.
+    %      
+    %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+    %        in MAXTAB and MINTAB are replaced with the corresponding
+    %        X-values.
+    %
+    %        A point is considered a maximum peak if it has the maximal
+    %        value, and was preceded (to the left) by a value lower by
+    %        DELTA.
+    
+    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+    % This function is released to the public domain; Any use is allowed.
+    
+    """
+    maxtab = []
+    mintab = []
+       
+    if x is None:
+        x = np.arange(len(v))
+    
+    v = np.asarray(v)
+    
+    if len(v) != len(x):
+        raise Exception('Input vectors v and x must have same length')
+    
+    if not np.isscalar(delta):
+        raise Exception('Input argument delta must be a scalar')
+    
+    if delta <= 0:
+        raise Exception('Input argument delta must be positive')
+    
+    mn, mx = np.Inf, -np.Inf
+    mnpos, mxpos = np.NaN, np.NaN
+    
+    lookformax = True
+    
+    for i in np.arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+        
+        if lookformax:
+            if this < mx-delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn+delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+ 
+    return np.array(maxtab), np.array(mintab)
+
+
+def LogPowerFFTAnalysis(powerBins, sampRate, threshold=10):
+    """
+    Analyze the power spectrum bins for noise floor and tones.
+    @param powerBins an array of real values FFT power bins
+    @param sampRate the sample rate for the FFT samples
+    @threshold how many dB above the noise floor to detect a tone
+    @return noiseFloor, [(tone0Freq, tone0Power), (tone1Freq, tone1Power), ]
+    """
+
+    #detect local minima and maxima
+    maxtab, mintab = peakdet(powerBins, 20)
+
+    #noise floor guess based on average of the log bins
+    noiseFloor = np.mean(powerBins)
+
+    #filter out peaks that are below the noise floor
+    peaks = list()
+    for i, v in maxtab:
+        if v < noiseFloor + threshold: continue
+        freq = (sampRate*i)/len(powerBins) - sampRate/2
+        peaks.append((freq, v))
+
+    return noiseFloor, peaks
